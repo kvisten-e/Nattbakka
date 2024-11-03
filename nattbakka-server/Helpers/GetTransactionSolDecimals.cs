185a86ea16c7679b6a294e8bb5d29bd1a956668d
Kvsiten <kvist.emil@gmail.com>
2024-11-03 13:41:04 +0100

    .

--- nattbakka-server/Helpers/GetTransactionSolDecimals.cs
+++ nattbakka-server/Helpers/GetTransactionSolDecimals.cs
@@ -6,24 +6,18 @@
         {
             char[] symbols = { '.', ',' };
             string solString = sol.ToString();
-            Console.WriteLine($"Sol: {sol}");
             int decimalsMax = 3;
             int firstDecimalIndex = solString.IndexOfAny(symbols);
-            Console.WriteLine($"firstDecimalIndex: {firstDecimalIndex}");
 
             if (firstDecimalIndex < 0)
             {
-                Console.WriteLine("Index är 0, retunerar 0");
                 return "0";
             }
             
             string solToString = sol.ToString();
-            Console.WriteLine($"solToString: {solToString}");
 
             string decimals = solToString.Split('.')[1];
-            Console.WriteLine($"decimals: {decimals}");
             decimals = (decimals.Length > 3) ? decimals[..decimalsMax] : decimals;
-            Console.WriteLine($"decimals2: {decimals}");
             return decimals;
         }
 
